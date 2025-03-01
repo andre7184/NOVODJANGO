9eb15c6d68821d702c2b6c9e42c61b747f004a69
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2025-02-28 21:37:46 -0400

    atualizado

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from .views.api_views import AlunoViewSet
 from .views.web_views import home
+from .views.web_views import CriarAluno
 
 router = DefaultRouter()
 router.register('alunos', AlunoViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    path('criarAluno/', CriarAluno, name='criarAluno'),
 ]
--- backend/api/views/web_views.py
+++ backend/api/views/web_views.py
@@ -4,3 +4,6 @@ from api.models import Aluno
 def home(request):
     alunos = Aluno.objects.all()
     return render(request, 'home.html', {'alunos': alunos})
+
+def CriarAluno(request):
+    return render(request, 'criar_aluno.html')
